c38f996c46b3e55848f4e54771b36e2456f31e43
PedroMou1 <96302672+PedroMou1@users.noreply.github.com>
2021-12-23 20:34:13 +0100

    Create reports.py

---
+++ reports.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import reportlab
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+
+# this code just generates a report with a determined style
+
+
+def generate_report(filename, title, info):
+ styles = getSampleStyleSheet()
+ report = SimpleDocTemplate(filename)
+ report_title = Paragraph(title, styles["h1"])
+ report_info = Paragraph(info, styles["BodyText"])
+
+ report.build([report_title, report_info])
+ return True
